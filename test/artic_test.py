c4d53629d7aeb9f2ddad181d5a4b3d69f801fdef
sniekum <sniekum@gmail.com>
2014-03-10 17:59:53 -0400

    Added BIC for global model size selection

--- test/artic_test.py
+++ test/artic_test.py
@@ -108,22 +108,22 @@ def makeDetectRequest(traj):
 if __name__ == '__main__':
     rospy.init_node('changepoint_test')
     
-    #stapler ex3: traj=2, sigmas=0.02, pi/30, penalty=100, min=20
-    #f = open('bagfiles/3-2-14/stapler2/ex4/diffpickle.txt', 'r')
+    #stapler ex3: traj=2, sigmas=0.02, pi/30, penalty=100, min=20 OR No penalty, but 1000/50 gauss time
+    f = open('bagfiles/3-2-14/stapler2/ex4/diffpickle.txt', 'r')
     
     #eraser ex6: traj=2, sigmas=0.005, pi/30, penalty=100, min=20
     #f = open('bagfiles/3-2-14/eraser/ex6/diffpickle.txt', 'r')
     
     #table ex3: traj=1, sigmas=0.02, pi/30, penalty=100, min=20, thresh=0.15 or .12 for traj2
-    f = open('bagfiles/3-2-14/tableshow/ex3/diffpickle.txt', 'r')
+    #f = open('bagfiles/3-2-14/tableshow/ex3/diffpickle.txt', 'r')
     
     [m1, m2, traj1, traj2] = pickle.load(f)
-    traj = traj1
+    traj = traj2
     
     #Remove points before obj contact
-    thresh = 0.15
-    include = [np.fabs(x[2]) < thresh for x in traj]
-    traj = [traj[i] for i in xrange(len(traj)) if include[i]==True]
+    #thresh = 0.15
+    #include = [np.fabs(x[2]) < thresh for x in traj]
+    #traj = [traj[i] for i in xrange(len(traj)) if include[i]==True]
     
     X = np.array([traj[i][0] for i in xrange(len(traj))])
     Y = np.array([traj[i][1] for i in xrange(len(traj))])
