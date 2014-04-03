6ac019501b3f7c96fd0e7f6274ac0a4635306f97
sniekum <sniekum@gmail.com>
2014-04-03 16:55:11 -0400

    Added more drawing tools to rvizArtic

--- test/rvizArtic.py
+++ test/rvizArtic.py
@@ -13,9 +13,7 @@ import drawUtils
 import arWorldModel
 import tf
 import geometry_msgs
-
-
-
+import numpy as np
 
 
 
@@ -27,7 +25,7 @@ def drawWedge(tag_id, radius, center, axis, min_q, max_q):
     tformer = tf.TransformerROS(True, rospy.Duration(10.0))
     
     while 1:
-        tag_pose = wm.getObjectById(0)
+        tag_pose = wm.getObjectById(tag_id)
         p0 = [0.0]*3
         p1 = [0.0]*3
         p2 = [0.0]*3
@@ -74,7 +72,110 @@ def drawWedge(tag_id, radius, center, axis, min_q, max_q):
         draw_utils.drawTriangle(p0,p1,p2)
         
         rospy.sleep(0.1)
+        
+        
+        
+def drawGraspAxis(tag_id, center, axis):
 
+    gen_utils = generalUtils.GeneralUtils()
+    draw_utils = drawUtils.DrawUtils()
+    wm = arWorldModel.ARWorldModel()
+    tformer = tf.TransformerROS(True, rospy.Duration(10.0))
+    
+    while 1:
+        tag_pose = wm.getObjectById(tag_id)
+
+        #First, make a frame for the base marker
+        m = geometry_msgs.msg.TransformStamped()
+        m.header.frame_id = 'torso_lift_link'
+        m.child_frame_id = 'base_marker_pose'
+        m.transform = gen_utils.vecToRosTransform(tag_pose)
+        tformer.setTransform(m)
+        
+        #Then, add a transform from base marker to axis of rotation
+        m = geometry_msgs.msg.TransformStamped()
+        m.header.frame_id = 'base_marker_pose'
+        m.child_frame_id = 'circ_axis_pose'
+        m.transform = gen_utils.vecToRosTransform(center+axis)
+        tformer.setTransform(m)
+        
+        #Now, calc coords in axis frame -- rotation axis is x-forward
+        coord1 =  geometry_msgs.msg.PoseStamped()
+        circ_axis.header.frame_id =  'circ_axis_pose'
+        c1 = [0, 0, 0, 0, 0, 0, 1]
+        coord1.pose = gen_utils.vecToRosPose(c1)
+        
+        #Finally, do the transforms to get in robot frame
+        try:
+            tp1 = tformer.transformPose('torso_lift_link',coord1)
+            grasp_axis = gen_utils.rosPoseToVec(tp1)
+        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
+            print "\n drawWedge(): TF transform problem!"
+        
+        draw_utils.drawGoal(grasp_axis)
+        
+        rospy.sleep(0.1)  
+        
+        
+        
+#traj is the (non-difference) traj of the marker of interest
+# tag_id is the tag to draw on
+def drawRigidDirections(tag_id, traj):
+    
+    gen_utils = generalUtils.GeneralUtils()
+    draw_utils = drawUtils.DrawUtils()
+    wm = arWorldModel.ARWorldModel()
+    tformer = tf.TransformerROS(True, rospy.Duration(10.0))            
+
+    #Just check and see if the observed movement in each DOF is greater than some epsilon
+    eps = 0.05 #meters  #FOR NOW, DON'T WORRY ABOUT ROTATIONAL DOFs
+    
+    traj_x = [traj[i][0] for i in xrange(len(traj))]
+    traj_y = [traj[i][0] for i in xrange(len(traj))]
+    traj_z = [traj[i][0] for i in xrange(len(traj))]
+    
+    draw_x = draw_y = draw_z = False
+    if max(traj_x) - min(traj_x) > eps:
+        draw_x = True
+    if max(traj_y) - min(traj_y) > eps:
+        draw_y = True
+    if max(traj_z) - min(traj_z) > eps:
+        draw_z = True
+    
+    while 1:
+        tag_pose = wm.getObjectById(tag_id)
+        tpq = tf.Quaternion(tag_pose[3:])  
+        
+        if draw_x:
+            rotate180z = tf.createQuaternionFromRPY(0,0,np.pi)
+            q2 = tpq * rotate180z
+            pose2 = tag_pos[0:3] + [q2.x, q2.y, q2.z, q2.w]
+            draw_utils.drawArrow(tag_pose)
+            draw_utils.drawArrow(pose2)
+            
+        if draw_y:
+            rotate90zpos = tf.createQuaternionFromRPY(0,0,np.pi/2.0)
+            rotate90zneg = tf.createQuaternionFromRPY(0,0,-np.pi/2.0)
+            q1 = tpq * rotate90zpos
+            q2 = tpq * rotate90zneg
+            pose1 = tag_pos[0:3] + [q1.x, q1.y, q1.z, q1.w]
+            pose2 = tag_pos[0:3] + [q2.x, q2.y, q2.z, q2.w]
+            draw_utils.drawArrow(pose1)
+            draw_utils.drawArrow(pose2)
+            
+        if draw_z:
+            rotate90ypos = tf.createQuaternionFromRPY(0.0, np.pi/2.0, 0.0)
+            rotate90yneg = tf.createQuaternionFromRPY(0,0, -np.pi/2.0, 0,0)
+            q1 = tpq * rotate90ypos
+            q2 = tpq * rotate90yneg
+            pose1 = tag_pos[0:3] + [q1.x, q1.y, q1.z, q1.w]
+            pose2 = tag_pos[0:3] + [q2.x, q2.y, q2.z, q2.w]
+            draw_utils.drawArrow(pose1)
+            draw_utils.drawArrow(pose2)
+            
+        rospy.sleep(0.1) 
+        
+        
   
 if __name__ == '__main__':
     
